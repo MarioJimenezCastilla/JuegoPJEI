62f1895f2514f2a8c6a71e7193d5b22d216a3e94
MarioJimenezCastilla <45268376+MarioJimenezCastilla@users.noreply.github.com>
2019-01-18 16:42:56 +0100

    Initial commit

---
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+
+    public int speed = 10;
+    public Vector3 posicionObjetivo;
+
+    private bool movimientoDerecha = true;
+    private bool bajando = false;
+    private bool movimientoIzquierda = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != posicionObjetivo){
+            transform.position = Vector2.MoveTowards(transform.position, posicionObjetivo, speed * Time.deltaTime);
+        } else {
+            
+            if (movimientoDerecha){
+                posicionObjetivo.y -= 2; 
+                bajando = true;
+            }
+
+
+        }
+        
+    }
+}
